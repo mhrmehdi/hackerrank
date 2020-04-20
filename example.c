ed50540303696e78d9cb9a4d1225c96d0f89a8ed
Mehdi Maharramov <mehdi.maharramov1@gmail.com>
2020-04-20 23:35:21 +0300

    Add files via upload

---
+++ example.c
@@ -0,0 +1,28 @@
+#include "loop.h"
+#include <stdio.h>
+
+char var[52];
+int start[52], end[52], step[52];
+int i=0;
+
+
+while(scanf("%c %d %d %d", &var[i], &start[i], &end[i], &step[i])){
+    i++;
+}
+
+
+int loop_variable_value(char c){
+    
+}
+
+
+
+void loop_continue(char c){
+    
+}
+
+
+int main(void){
+    
+    
+}
